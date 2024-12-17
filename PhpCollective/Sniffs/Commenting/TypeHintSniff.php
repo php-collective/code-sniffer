5540b49bf8568a067312bcd36b1442dd3b735183
mscherer <dereuromark@web.de>
2024-12-18 00:48:13 +0100

    Add warning for missing type hint.

--- PhpCollective/Sniffs/Commenting/TypeHintSniff.php
+++ PhpCollective/Sniffs/Commenting/TypeHintSniff.php
@@ -118,8 +118,6 @@ class TypeHintSniff extends AbstractSniff
             $tagComment = $phpcsFile->fixer->getTokenContent($tag + 2);
             $valueNode = static::getValueNode($tokens[$tag]['content'], $tagComment);
 
-
-
             if ($valueNode instanceof InvalidTagValueNode || $valueNode instanceof TypelessParamTagValueNode) {
                 if (!isset($valueNode->type)) {
                     $phpcsFile->addWarning('%s type hint is missing', $tag, 'MissingParamType', [$tokens[$tag]['content']]);
