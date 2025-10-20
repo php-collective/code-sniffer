f5437f0e61ee0970038bbe4ab1bc57bb85e2aa12
mscherer <dereuromark@web.de>
2025-10-20 19:29:09 +0200

    Add missing sniff ControlStructureEmptyStatement

---
+++ tests/PhpCollective/Sniffs/ControlStructures/ControlStructureEmptyStatementSniffTest.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * MIT License
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace PhpCollective\Test\PhpCollective\Sniffs\ControlStructures;
+
+use PhpCollective\Sniffs\ControlStructures\ControlStructureEmptyStatementSniff;
+use PhpCollective\Test\TestCase;
+
+class ControlStructureEmptyStatementSniffTest extends TestCase
+{
+    /**
+     * @return void
+     */
+    public function testControlStructureEmptyStatementSniffer(): void
+    {
+        $this->assertSnifferFindsErrors(new ControlStructureEmptyStatementSniff(), 5);
+    }
+
+    /**
+     * @return void
+     */
+    public function testControlStructureEmptyStatementFixer(): void
+    {
+        $this->assertSnifferCanFixErrors(new ControlStructureEmptyStatementSniff());
+    }
+}
---
+++ tests/_data/ControlStructureEmptyStatement/before.php
@@ -0,0 +1,30 @@
+<?php
+
+// Empty if statement
+if ($foo);
+
+// Empty foreach statement
+foreach ($items as $item);
+
+// Empty while statement
+while ($condition);
+
+// Empty for statement
+for ($i = 0; $i < 10; $i++);
+
+// Empty elseif statement
+if ($bar) {
+    echo "ok";
+} elseif ($baz);
+else {
+    echo "else";
+}
+
+// These are OK - have actual bodies
+if ($test) {
+    echo "good";
+}
+
+foreach ($array as $value) {
+    echo $value;
+}
