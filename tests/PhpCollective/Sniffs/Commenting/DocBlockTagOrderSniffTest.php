3f8129b4222946a2d264fee0cbd2f8bb04990c80
mscherer <dereuromark@web.de>
2026-05-09 15:22:11 +0200

    Extend DocBlockTagOrder to class/interface/trait docblocks
    Adds a configurable `classOrder` property (default: template, extends,
    implements, property, property-read, property-write, method, mixin) so
    class-level docblocks get the same consistent tag ordering that
    function/method docblocks already enforce. Makes the existing `order`
    property public so both can be overridden via XML configuration.

---
+++ tests/PhpCollective/Sniffs/Commenting/DocBlockTagOrderSniffTest.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * MIT License
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace PhpCollective\Test\PhpCollective\Sniffs\Commenting;
+
+use PhpCollective\Sniffs\Commenting\DocBlockTagOrderSniff;
+use PhpCollective\Test\TestCase;
+
+class DocBlockTagOrderSniffTest extends TestCase
+{
+    /**
+     * @return void
+     */
+    public function testDocBlockTagOrderSniffer(): void
+    {
+        $sniff = new DocBlockTagOrderSniff();
+        // One error per misordered docblock: class-level + function-level.
+        $this->assertSnifferFindsFixableErrors($sniff, 2, 2);
+    }
+
+    /**
+     * @return void
+     */
+    public function testDocBlockTagOrderFixer(): void
+    {
+        $sniff = new DocBlockTagOrderSniff();
+        $this->assertSnifferCanFixErrors($sniff, 2);
+    }
+
+    /**
+     * Custom $classOrder set via property override (mirrors XML configuration).
+     *
+     * @return void
+     */
+    public function testDocBlockTagOrderCustomClassOrder(): void
+    {
+        $this->prefix = 'custom-order.';
+
+        $sniff = new DocBlockTagOrderSniff();
+        $sniff->classOrder = ['@mixin', '@method', '@property', '@extends'];
+
+        $this->assertSnifferCanFixErrors($sniff, 1);
+
+        $this->prefix = null;
+    }
+
+    /**
+     * Tag names without leading "@" should be normalized.
+     *
+     * @return void
+     */
+    public function testDocBlockTagOrderCustomClassOrderWithoutAtPrefix(): void
+    {
+        $this->prefix = 'custom-order.';
+
+        $sniff = new DocBlockTagOrderSniff();
+        $sniff->classOrder = ['mixin', 'method', 'property', 'extends'];
+
+        $this->assertSnifferCanFixErrors($sniff, 1);
+
+        $this->prefix = null;
+    }
+}
---
+++ tests/_data/DocBlockTagOrder/custom-order.after.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types = 1);
+
+namespace PhpCollective;
+
+/**
+ * @mixin \Behavior
+ * @method \Foo bar()
+ * @property \Foo $X
+ * @extends \BaseTable
+ */
+class FixMe
+{
+}
